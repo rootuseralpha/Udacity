f6f27bfbbfbcb915c507640af31fc5dfa36ece67
Brent Markus <brent.markus@gmail.com>
2018-04-29 18:09:48 -0500

    blah

--- Linear Algebra Refresher Course/vector.py
+++ Linear Algebra Refresher Course/vector.py
@@ -139,16 +139,3 @@ class Vector(object):
 
     def __eq__(self, v):
         return self.coordinates == v.coordinates
-
-
-v = Vector([8.462, 7.893, -8.187])
-w = Vector([6.984, -5.975, 4.778])
-print(v.cross(w))
-
-v = Vector([-8.987, -9.838, 5.031])
-w = Vector([-4.268, -1.861, -8.866])
-print(v.area_of_parallelogram(w))
-
-v = Vector([1.5, 9.547, 3.691])
-w = Vector([-6.007, 0.124, 5.772])
-print(v.area_of_triangle(w))
